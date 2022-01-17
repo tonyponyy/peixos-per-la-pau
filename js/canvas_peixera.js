9568013b21e404e6eb55556e20af62e4f2ba2a35
Antoni  Andreu <antoniandreut@gmail.com>
2022-01-17 18:48:37 +0100

    noves imatges i optimització del rendiment de la peixera

--- js/canvas_peixera.js
+++ js/canvas_peixera.js
@@ -6,7 +6,6 @@ var peixos = [];
 bombolles = [];
 pinsos = [];
 counter = 0;
-friccio = 0;
 limitX1= 80
 limitX2= canvas.width-80;
 limitY1=60;
@@ -16,7 +15,7 @@ var peixera_canvas;
 var respuesta;
 var ANGULO = 32;
 class Peix {
-  constructor(imatge, x, y, zoom, dx, dy, angle) {
+  constructor(imatge, x, y, zoom, dx, dy, angle,data,id) {
     this.imatge = imatge;
     this.x = x;
     this.y = y;
@@ -24,6 +23,8 @@ class Peix {
     this.dx = dx;
     this.dy = dy;
     this.angle = angle;
+    this.data = data;
+    this.id = id;
   }
 }
 
@@ -80,6 +81,9 @@ bombolla_img.src = "imatges/bombolla.png";
 var alga = new Image();
 alga.src = "imatges/alga.png";
 
+var alga2_img = new Image();
+alga2_img.src = "imatges/alga2.png";
+
 var coral_img = new Image();
 coral_img.src = "imatges/coral.png";
 
@@ -119,6 +123,7 @@ function activar_peixera(id) {
 
     type: "get",
     success: function (response) {
+      console.log(response.peixos)
       tots_els_peixos = response.peixos;
       tots_els_peixos.forEach(function (peix) {
         var imatge_peix = new Image(800, 400);
@@ -129,7 +134,9 @@ function activar_peixera(id) {
         dx = Math.random() * 2 - Math.random() * 2;
         dy = Math.random() * 0.9 - Math.random() * 0.9;
         angle = Math.random() * 40;
-        var peix_a_guardar = new Peix(imatge_peix, x, y, zoom, dx, dy, angle);
+        data = transforma_data(peix.created_at);
+        id = peix.id;
+        var peix_a_guardar = new Peix(imatge_peix, x, y, zoom, dx, dy, angle,data,id);
         if (peix.visible) {
           peixos.push(peix_a_guardar);
         }
@@ -138,6 +145,7 @@ function activar_peixera(id) {
       peixera_canvas = ("000000" + response.peixera[0].fons.toString()).substr(
         5
       );
+      carrega_seed();
       moure_peixos();
     },
 
@@ -147,6 +155,16 @@ function activar_peixera(id) {
       );
     },
   });
+}
+
+function transforma_data(data){
+  any = data.substring(0,4);
+  mes = data.substring(5,7);
+  dia = data.substring(8,10);
+  hora = data.substring(11,13);
+  minuts = data.substring(14,16);
+  segons = data.substring(17,19);
+ return new Date(any,mes-1,dia,hora,minuts,segons);
 }
 
 function imprimir_peixos_index() {
@@ -182,17 +200,23 @@ function imprimir_peixos_index() {
         '<p class="targeta_peix_text"> Desitg : ' +
         tots_els_peixos[index].text +
         "</p>";
+      inner += '<p id="temps">Viu a la peixera des de '+ temps(transforma_data(tots_els_peixos[index].created_at)) +'.<p>'  
       inner += "</div>";
     }
   }
   document.getElementById("container_targetes").innerHTML = inner;
 }
 
+
+
+
+
+
 function moure_peixos() {
   //fons
 
-  fons();
-  ctx.save();
+ fons();
+  //ctx.save();
   onades();
   peixos.forEach(function (peix) {
    c_bombolles = debug? 100:1;
@@ -287,20 +311,20 @@ function moure_peixos() {
 
 
      // menjar pinsos
-     if(pinsos[0]){
+     if(pinsos[peix.id%9]){
       if (chance(5)){
-        py= pinsos[0].y
-        px= pinsos[0].x
+        py= pinsos[peix.id%9].y
+        px= pinsos[peix.id%9].x
         peix.dy= -(peix.y-py)/70
         peix.dx= -(peix.x-px)/70
       } 
     
 
 
-      if (peix.x < pinsos[0].x + 80 &&
-        peix.x + 80 > pinsos[0].x &&
-        peix.y < pinsos[0].y + 80 &&
-        80 + peix.y > pinsos[0].y) {
+      if (peix.x < pinsos[peix.id%9].x + 80 &&
+        peix.x + 80 > pinsos[peix.id%9].x &&
+        peix.y < pinsos[peix.id%9].y + 80 &&
+        80 + peix.y > pinsos[peix.id%9].y) {
           pinsos.splice(0, 1)
      }
 
@@ -310,25 +334,12 @@ function moure_peixos() {
      }
     
     drawRotatedImage(peix.imatge, peix.x, peix.y, -20 + peix.angle, peix.zoom,peix.dx);
-    ctx.restore();
+    //ctx.restore();
     
     //ctx.drawImage(peix.imatge, peix.x, peix.y, 800*peix.zoom, 400*peix.zoom);
 
     peix.x += peix.dx;
     peix.y += peix.dy;
-
-     if (peix.dx>0){
-       peix.dx -=friccio;
-     }
-     if (peix.dx<0){
-      peix.dx +=friccio;
-    }
-    if (peix.dy>0){
-      peix.dy -=friccio;
-    }
-    if (peix.dy<0){
-     peix.dy +=friccio;
-   }
 
 
     if (debug){
@@ -339,7 +350,7 @@ function moure_peixos() {
     
 
   });
-  ctx.restore();
+ // ctx.restore();
   for (let i = 0; i < bombolles.length; i++) {
     if (chance(20)) {
       bombolles[i].x += Math.random();
@@ -406,114 +417,142 @@ function onades(){
 
 
 Y = 200;
+
+var
+color1,
+color2,
+x1,
+y1,
+x2,
+y2,
+coral,
+posXc,
+castell,
+posXca,
+x3,
+y3,
+alga1 ,
+posX1 ,
+posY1,
+size1, 
+alga2 ,
+posX2,
+size2 ,
+posY2 ;
+
+
+function carrega_seed(){
+
+
+colors=["#368b8d","#36608d","#5688bd","#56bd86","#8656bd","#9883ce","#92d8c9","#7ceed5",
+        "#c888dd","#3abba6"]
+
+colors_fosc =  ["#a53abb","#653abb","#3b65ba","#2d778f","#1d4d5d","#1d365d","#166f6a","#2077a2","#2036a2","#2077a2"]
+colors_clars = ["#33d7bc","#334fd7","#33a0d7","#bf6bff","#ed85ff","#85ffab","#a3cde0","#bbbaee","#81beca","#b2a9db"]
+
+color2= colors_fosc[parseInt(peixera_canvas.substr(4, 1))]
+color1= colors_clars[parseInt(peixera_canvas.substr(3, 1))]
+
+//fons 2
+x1 = -(parseInt(peixera_canvas.substr(1, 2)) / 100) * canvas.width;
+  y1 = -(parseInt(peixera_canvas.substr(1, 3)) / 1000) * 50;
+//fons 3
+x2 = -(parseInt(peixera_canvas.substr(3, 2)) / 100) * canvas.width;
+y2 = -(parseInt(peixera_canvas.substr(4, 2)) / 100) * 100;
+//coral
+coral = parseInt(peixera_canvas.substr(2, 2));
+posXc = (parseInt(peixera_canvas.substr(3, 2)) / 100) * canvas.width;
+// castell
+castell = parseInt(peixera_canvas.substr(2, 1) + peixera_canvas.substr(6, 1));
+posXca =
+  (parseInt(peixera_canvas.substr(4, 1) + peixera_canvas.substr(1, 1)) /
+    100) *
+    canvas.width;
+//terra peixera
+  x3 = -(parseInt(peixera_canvas.substr(5, 2)) / 100) * 759;
+  y3 = (parseInt(peixera_canvas.substr(0, 2)) / 100) * 30;
+  //alga 1
+  alga1 = parseInt(peixera_canvas.substr(5, 2));
+  posX1 = (parseInt(peixera_canvas.substr(2, 2)) / 100) * 924 - 34;
+  posY1 = (parseInt(peixera_canvas.substr(5, 2)) / 100) * 120;
+  size1 = (parseInt(peixera_canvas.substr(4, 3)) / 1000) * 100 + 100;
+  //alga 2
+  alga2 = parseInt(peixera_canvas.substr(3, 2));
+  posX2 = (parseInt(peixera_canvas.substr(1, 2)) / 100) * 924 - 34;
+  size2 = (parseInt(peixera_canvas.substr(1, 3)) / 1000) * 100 + 100;
+  posY2 = (parseInt(peixera_canvas.substr(2, 2)) / 100) * 120;        
+}
+
+
+
+
+
+
 function fons() {
   //peixera_canvas = ("000000"+((parseInt(Math.random()*999999)).toString())).substr(5)
   //colors fons peixera
-  verd =
-    parseInt(peixera_canvas.substr(3, 2)) +
-    100 +
-    parseInt(peixera_canvas.substr(4, 2)) +
-    1;
-  blau = parseInt(peixera_canvas.substr(4, 2)) + 200;
-  vermell = parseInt(peixera_canvas.substr(4, 2)) / 2 + 130;
-  if (blau == NaN) {
-    blau = 255;
-  }
-  if (verd == NaN) {
-    verd = 255;
-  }
-  if (vermell == NaN) {
-    vermell = 255;
-  }
-  transparencia = 0.3 + parseInt(peixera_canvas.substr(2, 2)) / 100;
-  //console.log('rgba('+vermell+','+verd+','+blau+','+transparencia+')')
-  // ctx.fillStyle = 'rgba(144,'+verd+','+blau+','+transparencia+')';
+  
+  //console.log('rgba('+vermell+','+verd+','+blau+','+1+')')
+  // ctx.fillStyle = 'rgba(144,'+verd+','+blau+','+1+')';
 
   //ctex.createLinearGradient()
   grad1 = ctx.createLinearGradient(0, 0, 0, canvas.height);
-
+  //"rgba(" + vermell1 + "," + verd1 + "," + blau1 + "," + 11 * 2 + ")"
   grad1.addColorStop(
     0,
-    "rgba(" + vermell + "," + verd + "," + blau + "," + transparencia * 2 + ")"
+    
+    color1
   );
   grad1.addColorStop(
     1,
-    "rgba(" +
-      parseInt((vermell + verd) / 2) +
-      "," +
-      parseInt((blau + vermell) / 2) +
-      "," +
-      verd +
-      "," +
-      transparencia +
-      ")"
+    color2
   );
   ctx.fillStyle = grad1;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   //fons2
-  x = -(parseInt(peixera_canvas.substr(1, 2)) / 100) * canvas.width;
-  y = -(parseInt(peixera_canvas.substr(1, 3)) / 1000) * 50;
-  //ctx.fillStyle="ffffff";
-  //ctx.fillRect(100, 300, 1000, 1000);
+
+
   ctx.fillStyle = "#461556";
-  ctx.fillRect(0, 330 - y, canvas.width, canvas.height);
-  ctx.drawImage(fons2_img, x + 0, 50 - y);
-  ctx.drawImage(fons2_img, x+1000, 50 - y);
-  ctx.drawImage(fons2_img, x + 2000, 50 - y);
+  ctx.fillRect(0, 330 - y1, canvas.width, canvas.height);
+  ctx.drawImage(fons2_img, x1 + 0, 50 - y1);
+  ctx.drawImage(fons2_img, x1+1000, 50 - y1);
+  ctx.drawImage(fons2_img, x1 + 2000, 50 - y1);
   //fons1
-  x = -(parseInt(peixera_canvas.substr(3, 2)) / 100) * canvas.width;
-  y = -(parseInt(peixera_canvas.substr(4, 2)) / 100) * 100;
+ 
   //ctx.fillStyle="ffffff";
   //ctx.fillRect(100, 300, 1000, 1000);
   ctx.fillStyle = "#680a85";
-  ctx.fillRect(0, 410 - y, canvas.width, canvas.height);
-  ctx.drawImage(fons1_img, x + 0, 150 - y);
-  ctx.drawImage(fons1_img, x + 1000, 150 - y);
-  ctx.drawImage(fons1_img, x + 2000, 150 - y);
+  ctx.fillRect(0, 410 - y2, canvas.width, canvas.height);
+  ctx.drawImage(fons1_img, x2 + 0, 150 - y2);
+  ctx.drawImage(fons1_img, x2 + 1000, 150 - y2);
+  ctx.drawImage(fons1_img, x2 + 2000, 150 - y2);
   // coral
-  coral = parseInt(peixera_canvas.substr(2, 2));
-  posXc = (parseInt(peixera_canvas.substr(3, 2)) / 100) * canvas.width;
-  sizec = (parseInt(peixera_canvas.substr(3, 3)) / canvas.width) * 100 + 100;
-  posYc = (parseInt(peixera_canvas.substr(5, 2)) / 100) * 60;
+ 
   if (coral < 66) {
     //ctx.globalAlpha = 0.5;
-    ctx.drawImage(coral_img, posXc, canvas.height-420 + posYc, sizec, 400);
+    ctx.drawImage(coral_img, posXc, canvas.height-320 );
     //  ctx.globalAlpha = 1;
   }
 
   // castell
 
-  castell = parseInt(peixera_canvas.substr(2, 1) + peixera_canvas.substr(6, 1));
-  posXca =
-    (parseInt(peixera_canvas.substr(4, 1) + peixera_canvas.substr(1, 1)) /
-      100) *
-      canvas.width;
-
   if (castell < 33) {
     ctx.drawImage(castell_img, posXca, canvas.height-320, 300, 300);
   }
 
-  // terra_peixera
-  x = -(parseInt(peixera_canvas.substr(5, 2)) / 100) * 759;
-  y = (parseInt(peixera_canvas.substr(0, 2)) / 100) * 30;
-  ctx.drawImage(fons_peixera, x + 0, canvas.height-100 + y);
-  ctx.drawImage(fons_peixera, x + 1000, canvas.height-100 + y);
-  ctx.drawImage(fons_peixera, x + 2000, canvas.height-100 + y);
+//terra peixera
+  ctx.drawImage(fons_peixera, x3 + 0, canvas.height-100 + y3);
+  ctx.drawImage(fons_peixera, x3 + 999, canvas.height-100 + y3);
+  ctx.drawImage(fons_peixera, x3 + 1998, canvas.height-100 + y3);
   // alga 1
-  alga1 = parseInt(peixera_canvas.substr(5, 2));
-  posX1 = (parseInt(peixera_canvas.substr(2, 2)) / 100) * 924 - 34;
-  posY1 = (parseInt(peixera_canvas.substr(5, 2)) / 100) * 120;
-  size1 = (parseInt(peixera_canvas.substr(4, 3)) / 1000) * 100 + 100;
+
   if (alga1 < 50) {
-    ctx.drawImage(alga, posX1, 365 + 115+posY1, size1, 100);
+    ctx.drawImage(alga, posX1, 365 + 115+posY1);
   }
   // alga 2
-  alga2 = parseInt(peixera_canvas.substr(3, 2));
-  posX2 = (parseInt(peixera_canvas.substr(1, 2)) / 100) * 924 - 34;
-  size2 = (parseInt(peixera_canvas.substr(1, 3)) / 1000) * 100 + 100;
-  posY2 = (parseInt(peixera_canvas.substr(2, 2)) / 100) * 120;
+
   if (alga2 < 50) {
-    ctx.drawImage(alga, posX2, 365 + 115+posY2, size2, 100);
+    ctx.drawImage(alga2_img, posX2, 365 + 115+posY2);
   }
 }
 
@@ -596,6 +635,48 @@ function  angulo(x,y){
     }
   
 }
+
+function temps(data){
+
+  ara = Date.now()
+  segons = (ara - data)/1000 ;
+  if (segons< 60){
+    return "fa menys d'un minut"
+  }
+  if (segons< 15*60){
+    return "fa menys d'un quart d'hora"
+  }
+  if (segons< 60*60){
+    return "fa menys d'una hora"
+  }
+  if (segons< 24*3600){
+    return "fa menys de "+(parseInt(segons/3600)+1)+" hores"
+  }
+  if (segons< 48*3600){
+    return "fa un dia"
+  }
+  if (segons< (7*24)*3600){
+    return "fa "+parseInt((segons/3600)/24)+" dies"
+  }
+  if (segons< 2*(7*24)*3600){
+    return "fa "+parseInt(((segons/3600)/24)/7)+" setmana"
+  }
+  if (segons< (30*24)*3600){
+    return "fa "+parseInt(((segons/3600)/24)/7)+" setmanes"
+  }
+
+
+  if (segons< 12*((30*24)*3600)){
+    return "fa "+parseInt(((segons/3600)/24)/7)+" mesos"
+  }
+  if (segons> 12*((30*24)*3600)){
+    return "fa "+parseInt(12*(((segons/3600)/24)/7))+" anys"
+  }
+
+
+}
+
+
 
 debug = false;
 
